4466149ccfaa26424ae3a633afda0e03d35ad5b5
Tony Lian <1040424979@qq.com>
2020-12-26 14:01:52 +0800

    Add history

--- zhuangzhuangml/static/main.js
+++ zhuangzhuangml/static/main.js
@@ -6,6 +6,13 @@ define(['base/js/namespace',
     function get_code_output(i) {
         // i is the cell index
         let json = IPython.notebook.toJSON();
+        console.log(json);
+        let cell = IPython.notebook.get_cell(i);
+        if (cell.id === undefined) {
+            cell.metadata = {...cell.metadata, id: Math.floor(Math.random() * 10000000)};
+        }
+
+        console.log(json);
         return {
             "cell": json.cells[i],
             "code": json.cells[i].source,
@@ -15,11 +22,13 @@ define(['base/js/namespace',
 
     function init_set_bind() {
         //select the code cell for addition of the function
+
+        $(".new-git").remove();
         let cells = $(".cell");
         for(let i = 0; i < cells.length; i++){
             let cell = cells[i];
             cell = $(cell);
-            cell.prepend("<div><button class = \"git-btn btn btn-default\"></button></div>");
+            cell.prepend("<div class=\"new-git\"><button class = \"git-btn btn btn-default\"></button></div>");
         }
         let buttons = $(".git-btn");
         for(let i = 0; i < buttons.length;i++){
@@ -31,7 +40,22 @@ define(['base/js/namespace',
             let btn = buttons[i];
             btn = $(btn);
             btn.attr("index", i);
-            btn.click(() => {console.log(get_code_output(i));});
+            btn.click(() => {
+                let input = $(".input").eq(i);
+                console.log(input);
+                let h = input.find(".hist");
+                if (h.length) {
+                    h.remove();
+                } else {
+                    input.prepend(`<div class="hist">
+                        Example <br />
+                        Example <br />
+                        Example
+                    </div>`);
+                }
+
+                console.log(get_code_output(i));
+            });
         }
     }
 
@@ -41,12 +65,9 @@ define(['base/js/namespace',
         console.info('Loaded Jupyter extension: zhuangzhuangml');
 
         init_set_bind();
-
-        // register new action
-        var action_name = IPython.keyboard_manager.actions.register(git_commit_push, 'commit-push', 'jupyter-git')
-
-        // add button for new action
-        IPython.toolbar.add_buttons_group([action_name])
+        $("button").click(()=>{
+            init_set_bind();
+        });
 
     }
 
